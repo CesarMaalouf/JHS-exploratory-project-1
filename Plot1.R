ad7a9a49c6aa9028dcfa41e38717d3898349c95d
Cesar Maalouf <cesar.maalouf@gmail.com>
2015-04-12 19:58:49 +0200

    jhs exploratory project 1

---
+++ Plot1.R
@@ -0,0 +1,9 @@
+power <- read.csv("household_power_consumption.csv",header = TRUE,sep=";",fill = TRUE)
+#subsetting the file by filtering only the 2 dates (1/2/2007" , "2/2/2007")
+nedf <-subset(power, Date == c("1/2/2007" , "2/2/2007"))
+#Plotting the histogram
+with(nedf,hist(Global_active_power,main = "Global Active Power",xlab="Global Active Power (kilowatts)",col="red" ))
+#Copy my plot to a PNG file
+dev.copy(png, file = "Plot1.png")
+# Closing the PNG device
+dev.off()
